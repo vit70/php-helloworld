c75d307ef41f9fdee672a018980514cb0a7a0e26
root <root@master.example.com>
2020-02-29 12:37:22 +0100

    Changed index page contents.

--- index.php
+++ index.php
@@ -2,4 +2,5 @@
 print "Salve mondo! Php version is " . PHP_VERSION . "\n";
 print "Ho effettuato un cambiamento in treno \n";
 print "Ho effettuato un cambiamento a casa \n";
+print "Ho effettuato un cambiamento a casa l 29/2/2020\n";
 ?>
